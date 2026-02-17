8d21edfa8898e0485a92dd45a5c8199be9fd4a53
achien05 <achien@binghamton.edu>
2026-02-17 14:44:03 -0500

    edited makefile

--- submit/prj1-sol/make.sh
+++ submit/prj1-sol/make.sh
@@ -2,7 +2,9 @@
 
 # TODO: use this script to compile any artifacts needed by your parser,
 # assuming that the current directory is the one containing this file.
-
-echo "no build done"
+all:
+    echo "just run.sh"
+    ./run.sh
+    echo "no build done"
 
 
